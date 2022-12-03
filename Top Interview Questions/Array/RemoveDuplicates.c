1c54e86b51a8579da26d899438aafd2cd648473e
Kai <57695473+KaiHsuan-Liu@users.noreply.github.com>
2022-12-03 16:55:27 +0800

    Update RemoveDuplicates.c

--- Top Interview Questions/Array/RemoveDuplicates.c
+++ Top Interview Questions/Array/RemoveDuplicates.c
@@ -1,3 +1,31 @@
 /*
 LeetCode - 0026. Remove Duplicates from Sorted Array (Easy)
+URL: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 */
+
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize)
+{
+	int i = 0;
+	
+	for (int j = 1; j < numsSize; j++)
+	{
+		if (nums[j] != nums[i])
+		{
+			i++;
+			nums[i] = nums[j];
+		}
+	}
+	
+	return i+1;
+}
+
+int main(void)
+{
+	int nums[] = {1, 1, 2};
+	int k = sizeof(nums) / sizeof(nums[0]);
+	printf("Length: %d\n", removeDuplicates(nums, k));
+	
+	return 0;
+}
